8acb1264879d9d441764ed4d4c213ce9e0a2e5ff
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-03-09 19:32:39 +0300

    Add t1_n5_sequence_handler tests

--- t1_n5_sequence_handler/SequenceHandlerTester/SequenceHandlerTester.cpp
+++ t1_n5_sequence_handler/SequenceHandlerTester/SequenceHandlerTester.cpp
@@ -2,10 +2,36 @@
 //
 
 #include "stdafx.h"
+#include "../SequenceHandler/VectorProcessor.h"
 
-
-int _tmain(int argc, _TCHAR* argv[])
+bool CheckVectorProcessing(std::vector<double> const& in, std::vector<double> const& out)
 {
-	return 0;
+	std::vector<double> tmp(in);
+	ProcessVector(tmp);
+	return tmp == out;
 }
 
+bool VectorDoesNotChange(std::vector<double> const &a)
+{
+	return CheckVectorProcessing(a, a);
+}
+
+BOOST_AUTO_TEST_CASE(EmptyVectorProducesEmptyVector)
+{
+	BOOST_CHECK(VectorDoesNotChange({}));
+}
+
+BOOST_AUTO_TEST_CASE(VectorWithoutNegativesDoesntChangeContent)
+{
+	BOOST_CHECK(VectorDoesNotChange({ 4, 0, 3 }));
+}
+
+BOOST_AUTO_TEST_CASE(VectorWithOneNegativeElement)
+{
+	BOOST_CHECK(CheckVectorProcessing({ -1, 3 }, { 3, 3 }));
+}
+
+BOOST_AUTO_TEST_CASE(VectorWithSeveralNegativeElements)
+{
+	BOOST_CHECK(CheckVectorProcessing({ -1, -1, 2, -3 }, { 6, 6, 2, 18 }));
+}
