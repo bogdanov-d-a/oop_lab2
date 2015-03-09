8a6ef55b6ccfc6dc777b1aaf263fd5e839fb41aa
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-03-09 23:27:56 +0300

    Add t2_n1_extra_space_remover tests

--- t2_n1_extra_space_remover/ExtraSpaceRemoverTester/ExtraSpaceRemoverTester.cpp
+++ t2_n1_extra_space_remover/ExtraSpaceRemoverTester/ExtraSpaceRemoverTester.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include "../ExtraSpaceRemover/RemoveExtraSpacesMachine.h"
 
-
-int _tmain(int argc, _TCHAR* argv[])
+bool CheckConversion(std::string const& in, std::string const& out)
 {
-	return 0;
+	return CRemoveExtraSpacesMachine::RemoveExtraSpaces(in) == out;
 }
 
+bool StringDoesNotChange(std::string const& a)
+{
+	return CheckConversion(a, a);
+}
+
+BOOST_AUTO_TEST_CASE(SpacelessString)
+{
+	BOOST_CHECK(StringDoesNotChange("no-spaces-here"));
+}
+
+BOOST_AUTO_TEST_CASE(ModerateSpacesString)
+{
+	BOOST_CHECK(StringDoesNotChange("moderate spaces here"));
+}
+
+BOOST_AUTO_TEST_CASE(WideSpacesString)
+{
+	BOOST_CHECK(CheckConversion("wide      spaces                here", "wide spaces here"));
+}
+
+BOOST_AUTO_TEST_CASE(SpacesOnStart)
+{
+	BOOST_CHECK(CheckConversion("     str", "str"));
+}
+
+BOOST_AUTO_TEST_CASE(SpacesOnEnd)
+{
+	BOOST_CHECK(CheckConversion("str     ", "str"));
+}
+
+BOOST_AUTO_TEST_CASE(MixedString)
+{
+	BOOST_CHECK(CheckConversion("     too   much     data   to remove         ", "too much data to remove"));
+}
