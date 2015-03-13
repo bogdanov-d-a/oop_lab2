f9163848ba918f7658223f5e40d5bf2a36f14f01
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-03-13 15:44:00 +0300

    Add t3_n1_word_stat tests

--- t3_n1_word_stat/WordStatTester/WordStatTester.cpp
+++ t3_n1_word_stat/WordStatTester/WordStatTester.cpp
@@ -2,8 +2,65 @@
 //
 
 #include "stdafx.h"
+#include "../WordStat/CWordStat.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+bool RunTest(std::string const& str, CWordStat::Dict const& dict)
 {
-	return 0;
+	CWordStat::Dict result;
+	CWordStat wordStat(result);
+
+	for_each(str.cbegin(), str.cend(),
+		[&wordStat](char c){ wordStat.SendChar(c); });
+
+	wordStat.EndDataStream();
+	return (result == dict);
+}
+
+BOOST_AUTO_TEST_CASE(EmptyString)
+{
+	BOOST_CHECK(RunTest("", CWordStat::Dict()));
+}
+
+BOOST_AUTO_TEST_CASE(OneWord)
+{
+	BOOST_CHECK(RunTest("word",
+	{
+		{ "word", 1 } 
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(DifferentWords)
+{
+	BOOST_CHECK(RunTest("word1 word2 word3",
+	{
+		{ "word1", 1 },
+		{ "word2", 1 },
+		{ "word3", 1 }
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(SameWordLowerCase)
+{
+	BOOST_CHECK(RunTest("hello hello hello hello hello",
+	{
+		{ "hello", 5 }
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(SameWordDifferentCase)
+{
+	BOOST_CHECK(RunTest("hellO HEllO hEllO hello HeLLo",
+	{
+		{ "hello", 5 }
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(MultipleWords)
+{
+	BOOST_CHECK(RunTest("HEllO world hEllO WORlD hello word HeLLo WORD",
+	{
+		{ "hello", 4 },
+		{ "world", 2 },
+		{ "word", 2 }
+	}));
 }
