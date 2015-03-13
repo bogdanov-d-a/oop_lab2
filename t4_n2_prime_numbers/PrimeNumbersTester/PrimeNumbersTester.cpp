5925b8068ac66ec133df04d2d3fbb80edbf701e7
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-03-13 20:23:44 +0300

    Add t4_n2_prime_numbers tests

--- t4_n2_prime_numbers/PrimeNumbersTester/PrimeNumbersTester.cpp
+++ t4_n2_prime_numbers/PrimeNumbersTester/PrimeNumbersTester.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
+#include "../PrimeNumbers/PrimeNumbersUnit.h"
 
-
-int _tmain(int argc, _TCHAR* argv[])
+bool RunTest(PrimeNumbers::Number upperBound, PrimeNumbers::NumberSet const& primeNumberSet)
 {
-	return 0;
+	return (PrimeNumbers::GeneratePrimeNumbersSet(upperBound) == primeNumberSet);
 }
 
+BOOST_AUTO_TEST_CASE(EmptySet)
+{
+	BOOST_CHECK(RunTest(1, PrimeNumbers::NumberSet()));
+}
+
+BOOST_AUTO_TEST_CASE(UpToHundred)
+{
+	BOOST_CHECK(RunTest(100, {
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+		43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+	}));
+}
